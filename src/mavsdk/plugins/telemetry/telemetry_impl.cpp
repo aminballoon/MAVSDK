7e0da089e3cda61705801724a4319ddbc6fa6c50
Aminballoon <balloonkogoro@gmail.com>
2025-03-15 23:28:43 +0700

    fix style

--- src/mavsdk/plugins/telemetry/telemetry_impl.cpp
+++ src/mavsdk/plugins/telemetry/telemetry_impl.cpp
@@ -1120,9 +1120,10 @@ void TelemetryImpl::process_battery_status(const mavlink_message_t& message)
     new_battery.capacity_consumed_ah = (bat_status.current_consumed == -1) ?
                                            static_cast<float>(NAN) :
                                            bat_status.current_consumed * 1e-3f; // mAh to Ah
-    new_battery.time_remaining_s = (bat_status.time_remaining == std::numeric_limits<int16_t>::max()) ?
-                                       static_cast<float>(NAN) :
-                                       bat_status.time_remaining;
+    new_battery.time_remaining_s =
+        (bat_status.time_remaining == std::numeric_limits<int16_t>::max()) ?
+            static_cast<float>(NAN) :
+            bat_status.time_remaining;
 
     Telemetry::BatteryFunction battery_function;
     switch (bat_status.battery_function) {
